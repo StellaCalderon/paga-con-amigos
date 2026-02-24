d1a8e71f106e146630dbc0c70e3e3bd6bc0c49dc
StellaLaMejor <calderon.stella282@gmail.com>
2026-02-24 16:37:03 +0100

    Initial project

---
+++ PagaConAmigos/src/main/java/com/stellacm/pagaconamigos/PagaConAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.stellacm.pagaconamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagaConAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagaConAmigosApplication.class, args);
+	}
+
+}
---
+++ PagaConAmigos/src/test/java/com/stellacm/pagaconamigos/PagaConAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellacm.pagaconamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagaConAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
